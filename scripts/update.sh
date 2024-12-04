eb9bd01cb040a95952817047c23b7ce0a46b9f5c
Xingguang Yan <qheldiv@gmail.com>
2024-12-04 00:45:08 +0000

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@ cd ..
 git add .
 git commit -m "update"
 git push
-cd ../gruvi_site/
-git pull gruvica gh-pages --no-edit
-git push origin  gh-pages
+# cd ../gruvi_site/
+# git pull gruvica gh-pages --no-edit
+# git push origin  gh-pages
 
